157f92b052e0d3275956ec603afacfab2c4250f0
Bollar <916890457@qq.com>
2019-08-13 15:32:39 +0800

    创建User实体类

---
+++ src/main/java/com/pingco/pojo/User.java
@@ -0,0 +1,14 @@
+package com.pingco.pojo;
+
+/**
+ * @author : pingco
+ * create at:  2019/8/13  3:30 PM
+ * @description:
+ */
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+}
